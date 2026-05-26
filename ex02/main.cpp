67f17044278a12a1a7b54be97e3c7a3daa8096a6
ML <louism0495@gmail.com>
2026-05-26 19:14:02 +0200

    fix check

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,31 +2,51 @@
 #include <sstream>
 #include <limits>
 #include <vector>
+#include <exception>
 
-int main(int argc, char** argv)
+void	foo(char* args)
 {
-
-	if (argc != 2)
-	{
-		std::cerr << "Error\nUsage './PMergeMe \"<positive integers>\"'" << std::endl;
-		return 1;
-	}
-
 	std::vector<unsigned int>	cont1;
-	std::istringstream iss(argv[1]);
+	std::istringstream iss(args);
 	while (!iss.eof())
 	{
 		long val;
 		iss >> val;
 		if (iss.fail() || val < 0 || val > std::numeric_limits<int>::max())
 		{
-			throw std::runtime_error("Error: not a positive integer");
+			throw std::runtime_error("Error: argument contains a none positive integer");
 		}
 		cont1.push_back(static_cast<unsigned int> (val));
+	}
+}
+
+int main(int argc, char** argv)
+{
+
+	if (argc != 2 || argv[1][0] == 0)
+	{
+		std::cerr << "Erro: usage './PmergeMe \"<positive integers>\"'" << std::endl;
+		return 1;
+	}
+	try
+	{
+		foo(argv[1]);
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
 	}
 
 	return 0;
 }
+
+/**
+Group the elements of X into ⌊ n / 2 ⌋ pairs of elements, arbitrarily, leaving one element unpaired if there is an odd number of elements.
+Perform ⌊ n / 2 ⌋ comparisons, one per pair, to determine the larger of the two elements in each pair.
+Recursively sort the ⌊ n / 2 ⌋ larger elements from each pair, creating a sorted sequence S of ⌊ n / 2 ⌋ of the input elements, in ascending order, using the merge-insertion sort.
+Insert at the start of S the element that was paired with the first and smallest element of S.
+Insert the remaining ⌈ n / 2 ⌉ − 1  elements of X ∖ S into S, one at a time, with a specially chosen insertion ordering described below. Use binary search in subsequences of S(as described below) to determine the position at which each element should be inserted.
+ */
 
 /** 
 Your program must be able to use a positive integer sequence as an argument.
